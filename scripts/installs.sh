08d89ed41f996f1c3a9b3ea4eecb1fa30dda1687
Fergus Fettes <fergus.fettes@ondewo.com>
2020-08-16 22:20:07 +0100

    added some more install scripts for general stuff

--- scripts/installs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update -y
-sudo apt install -y stow jq parallel tmux zsh python3-neovim mosh
-
-echo add eternal terminal
---
+++ scripts/ubuntu_installs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt remove -y gnome-shell-extension-desktop-icons gnome-shell-extension-ubuntu-dock
+sudo apt install -y indicator-multiload
