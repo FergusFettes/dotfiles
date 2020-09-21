88aef5ac3ef345b05f89c2c26bd57c7cd02bd3aa
Fergus Fettes <fergus.fettes@ondewo.com>
2020-09-21 09:59:47 +0200

    added some more useful aliases etc

--- scripts/base_installs.sh
+++ scripts/base_installs.sh
@@ -2,6 +2,7 @@
 
 sudo apt update -y
 sudo apt install -y stow jq parallel tmux zsh python3-neovim mosh et
+sudo apt install -y cmake libfreetype6-dev libfontconfig1-dev xclip
 cargo install alacritty
 
 echo add eternal terminal
