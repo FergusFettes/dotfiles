0f3c83294aaf1e86d4c00c211ab0b0fc3328ff00
fergus <fergusfettes@gmail.com>
2021-04-04 19:30:14 +0200

    fix symlinks script

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-sudo ln -s /home/ffettes/.archive /a/
-sudo ln -s /home/ffettes/Downloads /d/
-sudo ln -s /home/ffettes/dotfiles /dt/
-sudo ln -s /home/ffettes/ /h/
-sudo ln -s /home/ffettes/javascript-playground /j/
-sudo ln -s /home/ffettes/personal /p/
-sudo ln -s /home/ffettes/personal /p/
-sudo ln -s /home/ffettes/.ssh /ss/
-sudo ln -s /home/ffettes/ondewo /w/
+sudo ln -s /home/ffettes/.archive /a
+sudo ln -s /home/ffettes/Downloads /d
+sudo ln -s /home/ffettes/dotfiles /dt
+sudo ln -s /home/ffettes/ /h
+sudo ln -s /home/ffettes/javascript-playground /j
+sudo ln -s /home/ffettes/personal /p
+sudo ln -s /home/ffettes/.ssh /ss
+sudo ln -s /home/ffettes/ondewo /w
