2b36877ca6cedeebc6cc3a9fe48178eac8e0865e
fergus <fergusfettes@gmail.com>
2022-02-03 16:34:48 +0200

    moving some scripts and adding home venv

--- scripts/purge_nvidia.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt --purge remove "*cublas*" "*cufft*" "*curand*" \
- "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*"
-
-sudo apt --purge remove "*nvidia*"
-
-sudo apt autoremove
