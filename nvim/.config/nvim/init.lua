e76ac52b30cc3140f4771fdcb45108dc670651e8
fergus <fergusfettes@gmail.com>
2023-07-17 10:26:46 +0100

    move nvim back

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,5 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require 'config.global'
+require 'config.lazy'
+require 'config.autocommands'
+require 'config.keymap'
+require 'config.functions'
